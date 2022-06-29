7ef54ad7d2e055052caa211586f2f0a327f2bc52
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-06-29 12:14:54 +0100

    Co-authored-by: Patrycja Miękina <PatMiekina@users.noreply.github.com>

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,133 +1,100 @@
 require './tictactoe'
 
-describe 'viewing the empty tic tac toe grid' do
-    # it 'remains empty' do
-    #     grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    #     expect(display_board(grid)).to eq(grid)
-    # end
-end
-
-describe '#choose_a_field' do
-    before(:each) do
-        @game = TicTacToe.new
-    end
-
-    it 'user input present' do
-        expect(@game.choose_a_field).not_to be_empty
-    end
-
-    # # 3x3 grid, rows: 1-3, columns 1-3
-    it 'returns an array' do
-        expect(@game.choose_a_field).to be_a_kind_of(Array)
-    end
-
-    it 'returns an array with two elements [row, column]' do
-        expect(@game.choose_a_field.size).to eq(2)
-    end
-
-    it 'row input is an integer' do
-        expect(@game.choose_a_field.first).to be_a_kind_of(Integer)
-    end
-
-    it 'column input is an integer' do
-        expect(@game.choose_a_field.last).to be_a_kind_of(Integer)
-    end
-
-    context 'row input is between 1-3' do
-        it 'row input is less or equal 3' do
-            expect(@game.choose_a_field(3).first).to be <= 3
-        end
-        it 'row input is 1 or more' do
-            expect(@game.choose_a_field(1).first).to be >= 1
-        end
-    end
-
-    context 'column input is between 1-3' do
-        it 'column input is less or equal 3' do
-            expect(@game.choose_a_field(3).last).to be <= 3
-        end
-        it 'column input is 1 or more' do
-            expect(@game.choose_a_field(1).last).to be >= 1
-        end
-    end
-end
-
 describe 'tic tac toe' do
     before(:each) do
         @game = TicTacToe.new
     end
 
-    it 'game is initialized with an empty board' do
-        expect(@game.board).to be_empty
+    context '#initialize' do
+        it 'game is initialized with an empty board' do
+            expect(@game.board).to eq([' ',' ',' ',' ',' ',' ',' ',' ',' '])
+        end
+
+        it 'player 1 uses "X"' do
+            expect(@game.player1).to eq("X")
+        end
+
+        it 'player 2 uses "O" (O, not zero)' do
+            expect(@game.player2).to eq("O")
+        end
+
+        it 'there is no winner' do
+            expect(@game.winner).to be nil
+        end
+
+        it 'initially the first element is empty' do
+            expect(@game.board.first).to eq(" ")
+        end
     end
 
-    it 'player 1 uses "X"' do
-        expect(@game.player1).to eq("X")
+    it '#current_player returns the player whose round it is' do
+        expect(@game.current_player).to eq("player1")
     end
 
-    it 'player 2 uses "O" (O, not zero)' do
-        expect(@game.player2).to eq("O")
+    it '#check_if_empty checks if field already in use' do
+        expect(@game.check_if_empty(1)).to be true
     end
 
-    it 'there is no winner' do
-        expect(@game.winner).to be nil
+    it '#switch a player switches a player' do
+        @game.switch_a_player
+        expect(@game.current_player).to eq("player2")
+    end
+
+    it 'expect #assign_a_value' do
+        @game.assign_a_value(@game.choose_a_field(1))
+        expect(@game.board.first).to eq("X")
+    end
+
+    it 'expect #assign_a_value not to override existing value' do
+        @game.assign_a_value(@game.choose_a_field(1))
+        @game.switch_a_player
+        @game.assign_a_value(@game.choose_a_field(1))
+        expect(@game.board.first).to eq("X")
+    end
+
+    context '#choose_a_field' do
+        it 'user input present' do
+            expect(@game.choose_a_field).not_to be nil
+        end
+
+        # # 3x3 grid, rows: 1-3, columns 1-3
+        it 'returns an array' do
+            expect(@game.choose_a_field).to be_a_kind_of(Integer)
+        end
+
+        context 'input is between 1-9' do
+            it 'input is less or equal 9' do
+                expect(@game.choose_a_field(3)).to be <= 9
+            end
+            it 'input is 1 or more' do
+                expect(@game.choose_a_field(1)).to be >= 1
+            end
+        end
+    end
+
+    context '#display_board' do
+        it 'displays an empty board' do
+            @game = TicTacToe.new
+            expect(@game.display_board).to eq(" 1 | 2 | 3 \n   |   |   \n   |   |   \n   |   |   \n 7 | 8 | 9 \n")
+        end
+    end
+
+    context "#winner" do
+        it 'there is no winner at the beginning' do
+            expect(@game.winner).to be nil
+        end
+    end
+
+    context "#check_victory_conditions" do
+        it 'the game is won if 3 symbols in a row' do
+            @game.assign_a_value(@game.choose_a_field(1))
+            @game.assign_a_value(@game.choose_a_field(2))
+            @game.assign_a_value(@game.choose_a_field(3))
+            expect(@game.check_victory_conditions).to be(true)
+        end
     end
 end
 
-describe "#check_win_status" do
-    before(:each) do
-        @game = TicTacToe.new
-    end
-
-    it 'there is no winner at the beginning' do
-        expect(@game.check_win_status).to be nil
-    end
-
-    # it 'there is no winner after round 1' do
-    # end
-end
-
-describe 'field' do #State
-    context 'has a content' do
-        it 'has an empty content' do
-            expect(Field.new(content: nil).content).to be nil
-        end
-
-        it 'has a content of "X"' do
-            expect(Field.new(content: "X").content).to eq("X")
-        end
-
-        it 'has a content of "O"' do
-            expect(Field.new(content: "O").content).to eq("O")
-        end
-    end
-
-    context 'it has a position' do
-        it 'row value is not nil' do
-            expect(Field.new(row: 1).row).not_to be nil
-        end
-
-        it 'column value is not nil' do
-            expect(Field.new(column: 1).column).not_to be nil
-        end
-
-        it 'row value is less or equal 3' do
-            expect(Field.new(row: 1).row).to be <= 3
-        end
-
-        it 'row value is more or equal 1' do
-            expect(Field.new(row: 1).row).to be >= 1
-        end
-
-        it 'column value is less or equal 3' do
-            expect(Field.new(column: 1).column).to be <= 3
-        end
-
-        it 'column value is more or equal 1' do
-            expect(Field.new(column: 1).column).to be >= 1
-        end
-    end
-end
 
 
 
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,29 +1,102 @@
 class TicTacToe
-    attr_accessor :board, :player1, :player2, :winner
+    attr_accessor :board, :player1, :player2, :winner, :players, :current_player
 
     def initialize()
-        @board = []
+        @board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
         @player1 = "X"
         @player2 = "O"
         @winner = nil
+        @players = ["player1", "player2"]
+        @current_player = @players[0]
     end
 
-    def display_board(cells)
-        cells
+    def switch_a_player
+        if @current_player == @players[0]
+            @current_player = @players[1]
+        elsif @current_player == @players[1]
+            @current_player = @players[0]
+        end
+    end
+
+    def display_board
+        " 1 | 2 | 3 \n #{self.board[0]} | #{self.board[1]} | #{self.board[2]} \n #{self.board[3]} | #{self.board[4]} | #{self.board[5]} \n #{self.board[6]} | #{self.board[7]} | #{self.board[8]} \n 7 | 8 | 9 \n"
     end
     
-    def choose_a_field(row = 1, column = 1)
+    def choose_a_field(field = 1)
         # get a row
         # put "Choose a row:"
         # row = gets.chomp
         # get a column
         # [row.to_i, column.to_i]
-        [row, column]
+        field
     end
 
-    def check_win_status
-        @winner
+    def check_if_empty(field)
+        self.board[field - 1] == " "
     end
+
+    def assign_a_value(field)
+        if @current_player = @players[0]
+            value = @player1
+        elsif @current_player = @players[1]
+            value = @player2
+        end
+        self.board[field - 1] = value# if check_if_empty(field)
+    end
+
+    def check_victory_conditions
+        victory_conditions = [  
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [6, 4, 2],
+        [2, 5, 8],
+        [1, 4, 7],
+        [0, 3, 6]
+        ]
+
+        # needs to return, otherwise just loops over the whole thing?
+        victory_conditions.each do |conditions|
+            return true if self.board[contitions[0]] == self.board[contitions[1]] && self.board[contitions[1]] == self.board[contitions[2]]
+        end
+
+        # OR
+        # #select? I don't think #find would work and also the way it would work it returns the combination rather than true/false
+        victory_conditions.find do |combination|
+            @board[combination[0]] == @board[combination[1]] && @board[combination[1]] == @board[combination[2]]
+        end
+    end
+
+    victory_conditions = [  
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [6, 4, 2],
+        [2, 5, 8],
+        [1, 4, 7],
+        [0, 3, 6]
+    ]
+
+    victory_conditions.each do |conditions|
+        self.board[contitions[0]] == self.board[contitions[1]] && self.board[contitions[1]] == self.board[contitions[2]]
+    end
+
+    self.winner = self.current_player if self.check_victory_conditions
+
+    
+    def winning_match
+        victory_conditions.find do |combination|
+            @board[combination[0]] == @board[combination[1]] && @board[combination[1]] == @board[combination[2]]
+        end
+    end
+
+    
+
+
+
+
 end
 
 class Field
@@ -35,3 +108,17 @@ class Field
         @column = attributes[:column]
     end
 end
+
+@game = TicTacToe.new
+
+# displayed_board = ""
+# " 1 | 2 | 3 \n
+#  #{@game.board[0]} | #{@game.board[1]} | #{@game.board[2]} \n
+#  #{@game.board[3]} | #{@game.board[4]} | #{@game.board[5]} \n
+#  #{@game.board[6]} | #{@game.board[7]} | #{@game.board[8]} \n
+#   7 | 8 | 9 \n"
+# displayed_board
+
+
+" 1 | 2 | 3 \n   |   |   \n   |   |   \n   |   |   \n 7 | 8 | 9 \n"
+" 1 | 2 | 3 \n   |   |   \n   |   |   \n   |   |   \n  7 | 8 | 9 "
